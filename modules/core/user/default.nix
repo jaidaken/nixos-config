bc0e7f67905d770962183688c3b10c8032f1ce6a
Jaidaken <jamiehewitt@pm.me>
2025-01-20 23:56:50 +0000

    idk

--- modules/core/user/default.nix
+++ modules/core/user/default.nix
@@ -13,7 +13,7 @@
     useGlobalPkgs = true;
     extraSpecialArgs = { inherit inputs username host; };
     users.${username} = {
-      imports = [ ./../../home/default.nix ];
+      imports = [ ./../../home ];
       home.username = "${username}";
       home.homeDirectory = "/home/${username}";
       home.stateVersion = "25.05";
